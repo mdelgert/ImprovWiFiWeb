3fdc0d75f7d6960f9f2e3ecdb5e1865b5a131d87
Matthew Elgert <mdelgert@yahoo.com>
2024-12-29 20:49:00 -0500

    Enhance Vite configuration: add dynamic base path handling for GitHub Pages and set GITHUB_REPOSITORY environment variable in deployment workflow

--- website/vite.config.mjs
+++ website/vite.config.mjs
@@ -1,20 +1,26 @@
 import { defineConfig } from 'vite';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
 
-export default defineConfig({
-  root: 'public',
-  build: {
-    outDir: '../dist',
-    emptyOutDir: true,
-  },
-  plugins: [
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'firmware/**/*', // Copy everything in public/firmware
-          dest: 'firmware', // Place it in dist/firmware
-        },
-      ],
-    }),
-  ],
+export default defineConfig(({ mode }) => {
+  const isGitHubPages = process.env.GITHUB_REPOSITORY;
+  const basePath = isGitHubPages ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}/` : '/';
+
+  return {
+    root: 'public',
+    build: {
+      outDir: '../dist',
+      emptyOutDir: true,
+    },
+    base: basePath,
+    plugins: [
+      viteStaticCopy({
+        targets: [
+          {
+            src: 'firmware/**/*',
+            dest: 'firmware',
+          },
+        ],
+      }),
+    ],
+  };
 });
