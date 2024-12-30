e92a3f32dafb88b358ef06b9cb4cf9e29934bb93
Matthew Elgert <mdelgert@yahoo.com>
2024-12-29 19:49:09 -0500

    Enhance deployment workflow: add caching for Node modules, clean dist folder before build, and update publish directory for GitHub Pages. Also, include vite-plugin-static-copy in setup instructions and refactor Vite configuration for improved clarity.

--- website/Setup.md
+++ website/Setup.md
@@ -2,6 +2,7 @@
 cd website
 npm init -y
 npm install vite --save-dev
+npm install vite-plugin-static-copy --save-dev
 npx vite dev #To preview and run site
 npx vite build #To build the site and see the ../dist folder
 npx vite serve #To testing the production build locally before deploying it
--- website/vite.config.js
+++ website/vite.config.js
@@ -1,3 +1,13 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  root: 'public',
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+  },
+});
+
 // import { defineConfig } from 'vite';
 
 // export default defineConfig(({ command }) => ({
@@ -25,18 +35,18 @@
 //   };
 // });
 
-import { defineConfig } from 'vite';
+// import { defineConfig } from 'vite';
 
-export default defineConfig(({ mode }) => {
-  const isGitHubPages = process.env.GITHUB_REPOSITORY;
-  const basePath = isGitHubPages ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}/` : '/';
+// export default defineConfig(({ mode }) => {
+//   const isGitHubPages = process.env.GITHUB_REPOSITORY;
+//   const basePath = isGitHubPages ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}/` : '/';
 
-  return {
-    root: 'public', // Development root
-    build: {
-      base: basePath, // Dynamically set base for GitHub Pages
-      outDir: '../_site', // Output to "_site" folder, ignored by Jekyll
-      emptyOutDir: true, // Clean output before building
-    },
-  };
-});
+//   return {
+//     root: 'public', // Development root
+//     build: {
+//       base: basePath, // Dynamically set base for GitHub Pages
+//       outDir: '../_site', // Output to "_site" folder, ignored by Jekyll
+//       emptyOutDir: true, // Clean output before building
+//     },
+//   };
+// });
