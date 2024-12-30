8194d37f5262e14ba7c2428f3fd336d6eaee9c9e
Matthew Elgert <mdelgert@yahoo.com>
2024-12-29 19:05:11 -0500

    Refactor Vite configuration: uncomment and streamline build settings for improved clarity and functionality

--- website/vite.config.js
+++ website/vite.config.js
@@ -1,29 +1,29 @@
-// import { defineConfig } from 'vite';
-
-// export default defineConfig(({ command }) => ({
-//   root: 'public',
-//   build: {
-//     outDir: '../dist',
-//     emptyOutDir: true,
-//   },
-//   base: command === 'serve' ? '/' : '/ImprovWiFiWeb/', // Use '/' for local dev and '/ImprovWiFiWeb/' for production
-// }));
-
 import { defineConfig } from 'vite';
 
-export default defineConfig(({ mode }) => {
-  const isGitHubPages = process.env.GITHUB_REPOSITORY; // Check if it's being built for GitHub Pages
-  const basePath = isGitHubPages ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}/` : '/';
+export default defineConfig(({ command }) => ({
+  root: 'public',
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+  },
+  base: command === 'serve' ? '/' : '/ImprovWiFiWeb/', // Use '/' for local dev and '/ImprovWiFiWeb/' for production
+}));
 
-  return {
-    root: 'public', // Specify "public" as the development root
-    build: {
-      base: basePath, // Use GitHub Pages repository path or root for local development
-      outDir: '../dist', // Ensure the build output is placed in "website/dist"
-      emptyOutDir: true, // Clean the output directory before building
-    },
-  };
-});
+// import { defineConfig } from 'vite';
+
+// export default defineConfig(({ mode }) => {
+//   const isGitHubPages = process.env.GITHUB_REPOSITORY; // Check if it's being built for GitHub Pages
+//   const basePath = isGitHubPages ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}/` : '/';
+
+//   return {
+//     root: 'public', // Specify "public" as the development root
+//     build: {
+//       base: basePath, // Use GitHub Pages repository path or root for local development
+//       outDir: '../dist', // Ensure the build output is placed in "website/dist"
+//       emptyOutDir: true, // Clean the output directory before building
+//     },
+//   };
+// });
 
 
 
