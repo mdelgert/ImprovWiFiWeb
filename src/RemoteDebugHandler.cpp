b7271c99b9cba3e5d6522af6fc45cea96ce830ab
Matthew Elgert <mdelgert@yahoo.com>
2024-12-31 01:59:52 -0500

    Refactor RemoteDebugHandler: add comments to clarify potential interference with ImprovWifi during setup

--- src/RemoteDebugHandler.cpp
+++ src/RemoteDebugHandler.cpp
@@ -6,7 +6,12 @@ RemoteDebug Debug;
 void RemoteDebugHandler::init()
 {
     Serial.begin(115200);           // Start the serial communication
+
+    //#############################################
+    // This might be interfereing with ImprovWifi during setup...........................
     Debug.setSerialEnabled(true);   // Enable sending to serial debug as well (may want to disable it for release)
+    //#############################################
+
     Debug.setResetCmdEnabled(true); // Enable the reset command
     Debug.showProfiler(true);       // Profiler (Good to measure times, to optimize codes)
     Debug.showColors(true);         // Enable colors
