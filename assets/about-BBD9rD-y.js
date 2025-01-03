329435b0135a82d2f93de8c4567ccd2679c218e3
mdelgert <mdelgert@users.noreply.github.com>
2025-01-03 03:34:13 +0000

    deploy: 041e548acfa3ba2e800740a65fbe800e5c803acf

--- assets/about-BBD9rD-y.js
+++ assets/about-BBD9rD-y.js
@@ -1,4 +1,4 @@
-import"./style-M47sceeL.js";document.querySelector("#app").innerHTML=`
+import"./style-Cif0AnDf.js";document.querySelector("#app").innerHTML=`
   <div>
     <h1>About</h1>
     <p>Web Installer</p>
