ba25a8ef9faaebb46f6f443ea17b7b41b54b0991
Matthew Elgert <mdelgert@yahoo.com>
2025-01-10 13:04:04 -0500

    Refactor ButtonHandler: streamline long press handling and remove unused variables

--- include/ButtonHandler.h
+++ include/ButtonHandler.h
@@ -6,29 +6,18 @@
 #include "LedHandler.h"
 #include "GfxHandler.h"
 #include "ConfigManager.h"
+#include "NonBlockingTimer.h"
 
 class ButtonHandler {
 public:
-    // Initialize the button
     static void init();
-
-    // Update the button state (call this in the main loop)
     static void loop();
 
 private:
-    // OneButton instance for handling button events
     static OneButton button;
-
-    // Button action handlers
     static void handleSingleClick();
     static void handleDoubleClick();
-    static void handleLongPressStart();
-    static void handleDuringLongPress();
-    static void handleLongPressStop();
-
-    // Variables for custom long press actions
-    static unsigned long holdStartTime;
-    static bool countdownStarted;
+    static void handleLongPress();
 };
 
 #endif // BUTTON_HANDLER_H
