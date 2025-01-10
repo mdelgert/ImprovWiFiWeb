dc9b2b40a252d83927bad732f8a94659600dc2ce
Matthew Elgert <mdelgert@yahoo.com>
2025-01-10 13:40:54 -0500

    Refactor: remove MainIncludes.h and adjust includes in Main.cpp; update build script

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,4 +1,14 @@
-#include "MainIncludes.h"
+#include "Globals.h"
+#include <Arduino.h>
+#include "NonBlockingTimer.h"
+#include "RemoteDebugHandler.h"
+#include "ImprovHandler.h"
+#include "GfxHandler.h"
+#include "LedHandler.h"
+#include "ButtonHandler.h"
+#include "WifiHandler.h"
+#include "WebHandler.h"
+#include "ConfigManager.h"
 
 void setup()
 {
