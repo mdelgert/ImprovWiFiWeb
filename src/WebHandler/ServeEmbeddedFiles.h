36dc5a8d3e939e39b5da2e7a2b441bfd9e2badb8
Matthew Elgert <mdelgert@yahoo.com>
2025-01-10 21:02:41 -0500

    Remove favicon.ico embedding and update documentation for manual embedding approach

--- src/WebHandler/ServeEmbeddedFiles.h
+++ src/WebHandler/ServeEmbeddedFiles.h
@@ -18,16 +18,6 @@ extern const uint8_t advanced_js_end[] asm("_binary_web_advanced_js_end");
 extern const uint8_t error_html_start[] asm("_binary_web_error_html_start");
 extern const uint8_t error_html_end[] asm("_binary_web_error_html_end");
 
-//Not working - https://stackoverflow.com/questions/68924676/how-to-upload-binary-data-html-favicon-to-esp32-using-esp-idf
-//https://docs.espressif.com/projects/esp-idf/en/latest/esp32/api-guides/build-system.html#embedding-binary-data
-//https://community.platformio.org/t/how-to-embed-binary-files-into-esp32-firmware/3791/2
-//https://community.platformio.org/t/how-to-embed-binary-files-into-esp32-firmware/3791/6
-//https://docs.platformio.org/en/latest/platforms/espressif32.html#embedding-binary-data
-//./xtensa-esp32s3-elf-nm /mnt/d1/home/mdelgert/source/ImprovWiFiWeb/.pio/build/esp32-s3-devkitc-1/firmware.elf | grep favicon
-
-extern const uint8_t favicon_ico_start[] asm("_binary_favicon_ico_start");
-extern const uint8_t favicon_ico_end[] asm("_binary_favicon_ico_end");
-
 extern const uint8_t footer_html_start[] asm("_binary_web_footer_html_start");
 extern const uint8_t footer_html_end[] asm("_binary_web_footer_html_end");
 
@@ -58,3 +48,17 @@ extern const uint8_t terminal_js_start[] asm("_binary_web_terminal_js_start");
 extern const uint8_t terminal_js_end[] asm("_binary_web_terminal_js_end");
 
 #endif // EMBEDDED_FILES_H
+
+//Embedding binary files not working with current toolchain
+//Not working - https://stackoverflow.com/questions/68924676/how-to-upload-binary-data-html-favicon-to-esp32-using-esp-idf
+//https://community.platformio.org/t/add-t-none-or-t-binary/441/17
+//https://docs.espressif.com/projects/esp-idf/en/latest/esp32/api-guides/build-system.html#embedding-binary-data
+//https://community.platformio.org/t/how-to-embed-binary-files-into-esp32-firmware/3791/2
+//https://community.platformio.org/t/how-to-embed-binary-files-into-esp32-firmware/3791/6
+//https://docs.platformio.org/en/latest/platforms/espressif32.html#embedding-binary-data
+//https://github.com/platformio/platform-espressif32/issues/245
+//./xtensa-esp32s3-elf-nm /mnt/d1/home/mdelgert/source/ImprovWiFiWeb/.pio/build/esp32-s3-devkitc-1/firmware.elf | grep favicon
+
+//extern const uint8_t favicon_ico_start[] asm("_binary_favicon_ico_start");
+//extern const uint8_t favicon_ico_end[] asm("_binary_favicon_ico_end");
+//serveEmbeddedFile(server, "/favicon.ico", favicon_ico_start, favicon_ico_end, "image/x-icon");
