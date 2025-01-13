9c9ee941e6ca92e72203800db2fa9acbdfa41989
Matthew Elgert <mdelgert@yahoo.com>
2025-01-12 22:46:25 -0500

    Update SOFTWARE_VERSION to V1.0.14 in Globals.h; clean up Globals1.h

--- temp/src/Globals1.h
+++ temp/src/Globals1.h
@@ -1,10 +1,16 @@
 #pragma once
 
+//constexpr const char* DEFAULT_TIMEZONE = "America/New_York";
 #define DEFAULT_TIMEZONE "America/New_York";
-#define SOFTWARE_VERSION "V1.0.14"
+#define SOFTWARE_VERSION "V1.0.13"
 
+//#include "Secure.h"
 #include "SecureExample.h"
 #include "ConfigManager.h"
 #include "NonBlockingTimer.h"
 #include "RemoteDebugHandler.h"
 #include "BoardPins.h"
+
+
+
+
