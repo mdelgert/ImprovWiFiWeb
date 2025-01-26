2592eff893631fdb939762b2578a38678b47950a
Matthew Elgert <mdelgert@yahoo.com>
2025-01-26 17:54:52 -0500

    Comment out uploadfs target in build script to prevent unintended uploads

--- tools/build.sh
+++ tools/build.sh
@@ -2,6 +2,6 @@
 cd ..
 #pio run
 pio run --target upload
-pio run --target uploadfs
+#pio run --target uploadfs
 #pio remote --agent pve1 run --target upload
 #pio remote --agent d1 run --target upload
