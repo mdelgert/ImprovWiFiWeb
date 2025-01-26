8a21a68654be6167e1115d95473ab00587506821
Matthew Elgert <mdelgert@yahoo.com>
2025-01-26 18:46:04 -0500

    Update backup.sh to include extraction of backup.tar and comment out restore functionality

--- tools/backup.sh
+++ tools/backup.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 curl http://demo1.local/backup --output backup.tar
+
 #curl -X POST -F "file=@backup.tar" http://demo1.local/restore
+
+# Untar the file
+#tar -xzvf backup.tar
+tar -xvf backup.tar
