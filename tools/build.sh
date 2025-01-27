90a22b3b8b8902acea3187a47dbba349f2007586
Matthew Elgert <mdelgert@yahoo.com>
2025-01-27 02:35:09 -0500

    Update build scripts and configuration for flash size adjustments

--- tools/build.sh
+++ tools/build.sh
@@ -7,3 +7,4 @@ pio run --target uploadfs
 pio run --target upload
 #pio remote --agent pve1 run --target upload
 #pio remote --agent d1 run --target upload
+#esptool.py --chip esp32-s3 --port /dev/ttyACM0 --baud 921600 write_flash 0x1000 ../.pio/build/esp32-s3-devkitc-1/device.bin
